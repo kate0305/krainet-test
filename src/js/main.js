cc62f40bb0f9b7cd0aa9533eb3c4cd0daa1d5842
Katsiaryna Voronich <katarinka403@gmail.com>
2024-06-26 14:01:47 +0300

    feat: implement benefit section

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,8 @@ import '../styles/style.css';
 import '../styles/shared.css';
 import '../styles/promo.css';
 import '../styles/menu.css';
+import '../styles/content.css';
+import '../styles/benefits.css';
 
 import { openMenu, closeMenu } from '../js/open-menu';
 
